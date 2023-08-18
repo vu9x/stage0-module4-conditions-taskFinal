06115127ed3a92566cfef01f6d657e23603f112c
Vu <nguyentuanvu9x@gmail.com>
2023-08-18 15:31:48 +0300

    Adding: SeasonDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+        if (monthNumber <= 0 || monthNumber > 12) {
+            System.out.println("Wrong month number");
+        } else if (monthNumber <=2 || monthNumber == 12) {
+            System.out.println("Winter");
+        } else if (monthNumber >= 3 && monthNumber <= 5) {
+            System.out.println("Spring");
+        } else if (monthNumber >= 6 && monthNumber <= 8) {
+            System.out.println("Summer");
+        } else {
+            System.out.println("Autumn");
+        }
     }
 }
